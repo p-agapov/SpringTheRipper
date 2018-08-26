ba218a02d35864fbddacf940dd5ddfcf98660586
p-agapov <p.a.agapov@gmail.com>
2018-08-26 23:55:51 +0300

    Add field random int injection annotation

--- src/main/java/com/agapovp/springtheripper/quoters/TerminatorQuoter.java
+++ src/main/java/com/agapovp/springtheripper/quoters/TerminatorQuoter.java
@@ -4,11 +4,16 @@ import lombok.Setter;
 
 public class TerminatorQuoter implements Quoter {
 
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     @Setter
     private String message;
 
     @Override
     public void sayQuote() {
-        System.out.println(String.format("message = %s", message));
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(String.format("message = %s", message));
+        }
     }
 }
