9287fec743d866a41b84c7471dcb973efd00db4e
p-agapov <p.a.agapov@gmail.com>
2018-09-08 17:39:10 +0300

    Add Screensaver

---
+++ src/main/java/com/agapovp/springtheripper/screensaver/Config.java
@@ -0,0 +1,40 @@
+package com.agapovp.springtheripper.screensaver;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.*;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "com.agapovp.springtheripper.screensaver")
+public class Config {
+    @Bean
+    @Scope("prototype")
+    public Color color() {
+        Random random = new Random();
+        return new Color(
+                random.nextInt(255),
+                random.nextInt(255),
+                random.nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame frame() {
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(500);
+        }
+    }
+}
