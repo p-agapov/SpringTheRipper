d1d3204cf46969c6c4919a1cc245aa273d29dd45
p-agapov <p.a.agapov@gmail.com>
2018-09-08 15:53:56 +0300

    Add DeprecationHandlerBeanFactoryPostProcessor

---
+++ src/main/java/com/agapovp/springtheripper/quoters/T1000.java
@@ -0,0 +1,8 @@
+package com.agapovp.springtheripper.quoters;
+
+public class T1000 extends TerminatorQuoter implements Quoter {
+    @Override
+    public void sayQuote() {
+        System.out.println("Я ЖИДКИЙ");
+    }
+}
